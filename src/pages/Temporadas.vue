800d2602e2d8abc5458d88e43b91631f5c06db65
Átila Camurça <camurca.home@gmail.com>
2020-06-02 10:49:15 -0300

    Imagens das temporadas

--- src/pages/Temporadas.vue
+++ src/pages/Temporadas.vue
@@ -13,7 +13,7 @@
         <div
           v-for="i in 10"
           :key="i"
-          class="lg:w-1/3 sm:w-1/2 p-4"
+          class="lg:w-1/4 sm:w-1/2 p-4"
         >
           <div>
             <g-link
@@ -22,10 +22,10 @@
             >
               <g-image
                 :alt="`Temporada ${i}`"
-                class="absolute inset-0 w-full h-full object-cover object-center"
+                class="absolute inset-0 w-full h-full object-cover object-center rounded-lg"
                 :src="`/temporadas/thumbnail-${i}.jpg`"
               ></g-image>
-              <div class="px-8 py-48 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-75">
+              <div class="px-8 py-40 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-75">
                 <h2 class="tracking-widest text-center text-4xl title-font font-bold text-blue-600 mb-1">
                   Temporada {{ i }}
                 </h2>
