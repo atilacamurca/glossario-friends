c81fd9d895ebfd8290ddb83d4dc2bace6b76264f
Átila Camurça <camurca.home@gmail.com>
2020-06-03 12:31:33 -0300

    S01E01 - My Favorite Things
    Fixed #8.

--- src/components/global/Dialogo.vue
+++ src/components/global/Dialogo.vue
@@ -1,5 +1,5 @@
 <template functional>
-  <div class="markdown-dialogo flex sm:flex-row flex-col mt-4 mb-6">
+  <div class="markdown-dialogo flex sm:flex-row flex-col mt-4 mb-4">
     <div class="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center flex-shrink-0">
       <g-image
         :src="props.imagem"
