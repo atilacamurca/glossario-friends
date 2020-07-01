f76fbfa84d6f37f3892237cb92a7b259e8157fd0
Átila Camurça <camurca.home@gmail.com>
2020-07-01 11:41:15 -0300

    S01E13 - Sting
    Fixed #110.

--- src/components/global/CodEpisodio.vue
+++ src/components/global/CodEpisodio.vue
@@ -1,5 +1,5 @@
 <template functional>
-  <div class="inline-block py-1 px-3 rounded bg-orange-200 text-orange-600 text-md font-semibold tracking-wide mb-2">
+  <div class="inline-block py-1 px-3 rounded-full bg-orange-200 text-orange-600 text-md font-semibold tracking-wide mb-2">
     {{ props.codEpisodio }}
   </div>
 </template>
