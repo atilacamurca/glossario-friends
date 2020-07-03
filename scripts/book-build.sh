366c8cb4580436e76f86e6251013f1afd9759c9c
Átila Camurça <camurca.home@gmail.com>
2020-07-03 18:00:35 -0300

    Suporte a imagens full page
    Tamanho das outras imagens automaticamente definido

--- scripts/book-build.sh
+++ scripts/book-build.sh
@@ -9,7 +9,12 @@ set -o pipefail
 
 # compile episodes
 mkdir -p ./book/S01
-pandoc -f markdown -o ./book/S01/S01E01.tex ./temporadas/S01/S01E01.md
+pandoc \
+  -f markdown \
+  -o ./book/S01/S01E01.tex \
+  ./temporadas/S01/S01E01.md \
+  --filter ./scripts/pandoc-filter/bin.js
+  # --filter pandoc-fignos -M fignos-caption-name=Figura
 
 cd book
 pdflatex -shell-escape -interaction=nonstopmode main.tex
---
+++ scripts/pandoc-filter/filter.js
@@ -0,0 +1,51 @@
+var pandoc = require('pandoc-filter-promisified')
+const fromPairs = require('lodash.frompairs')
+const { paramCase } = require('param-case')
+const { Str, RawInline } = pandoc
+
+async function action (elt, format, meta) {
+  if (elt.t === 'Image') {
+    const optionsArray = elt.c[0]
+    const captionArray = elt.c[1]
+    const figureArray = elt.c[2]
+
+    const filepath = figureArray[0]
+    const caption = captionArray.reduce(function (prevVal, currVal, idx) {
+      if (currVal.t === 'Space') return prevVal + ' '
+      if (currVal.t === 'Str') return prevVal + currVal.c
+    }, '')
+
+    const imgOptions = fromPairs(optionsArray[2])
+    if (!imgOptions.fullpage) {
+      return toFigure(filepath, caption)
+    }
+
+    return toFigureFullPage(filepath, caption, imgOptions)
+  }
+}
+
+function toFigureCaption(caption) {
+  return `\\caption{${caption}\\label{fig:${paramCase(caption)}}}`
+}
+
+function toFigureFullPage (filepath, caption, { clipb, clipt}) {
+  return RawInline('latex', `\\begin{figure}[!ht]
+  \\begin{adjustwidth}{-\\oddsidemargin-1in}{-\\rightmargin}
+    \\centering
+    \\includegraphics[trim={0 ${clipb} 0 ${clipt}}, clip, width=\\paperwidth]{${filepath}}
+    ${toFigureCaption(caption)}
+  \\end{adjustwidth}
+\\end{figure}`)
+}
+
+function toFigure (filepath, caption) {
+  return RawInline('latex', `\\begin{figure}
+  \\centering
+  \\includegraphics[width=\\columnwidth]{${filepath}}
+  ${toFigureCaption(caption)}
+\\end{figure}`)
+}
+
+module.exports = function () {
+  pandoc.stdio(action)
+}
