4b01becb1db28917c609078f2f048bef04c7e608
Átila Camurça <camurca.home@gmail.com>
2020-07-17 18:53:07 -0300

    Capítulo S01E05 do livro

--- scripts/book-build.sh
+++ scripts/book-build.sh
@@ -7,34 +7,19 @@ set -o pipefail
 # bibtex main
 
 # compile episodes
+
+# season 1
 mkdir -p ./book/S01
-pandoc \
+for i in $(seq -f "%02g" 1 5)
+do
+  echo "Gerando tex do episódio S01E${i} ..."
+  pandoc \
   -f markdown \
   --shift-heading-level-by=-1 \
-  -o ./book/S01/S01E01.tex \
-  ./temporadas/S01/S01E01.md \
+  -o "./book/S01/S01E${i}.tex" \
+  "./temporadas/S01/S01E${i}.md" \
   --filter ./scripts/pandoc-filter/bin.js
-
-pandoc \
-  -f markdown \
-  --shift-heading-level-by=-1 \
-  -o ./book/S01/S01E02.tex \
-  ./temporadas/S01/S01E02.md \
-  --filter ./scripts/pandoc-filter/bin.js
-
-pandoc \
-  -f markdown \
-  --shift-heading-level-by=-1 \
-  -o ./book/S01/S01E03.tex \
-  ./temporadas/S01/S01E03.md \
-  --filter ./scripts/pandoc-filter/bin.js
-
-pandoc \
-  -f markdown \
-  --shift-heading-level-by=-1 \
-  -o ./book/S01/S01E04.tex \
-  ./temporadas/S01/S01E04.md \
-  --filter ./scripts/pandoc-filter/bin.js
+done
 
 cd book
 pdflatex -shell-escape -interaction=nonstopmode main.tex
