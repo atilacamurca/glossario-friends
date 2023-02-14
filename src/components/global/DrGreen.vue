5e5904a1b93046ba0b0ba3de99f87be8ab81d674
Átila Camurça <camurca.home@gmail.com>
2023-02-14 18:12:20 -0300

    S03E07 - Ixtapa e Susan Lucci
    close #180

---
+++ src/components/global/DrGreen.vue
@@ -0,0 +1,24 @@
+<template functional>
+    <dialogo
+      :imagem="require('~/assets/img/dr-green.png')"
+      personagem="Dr. Green"
+      cor-fundo="bg-indigo-200"
+      :original="props.original"
+      :traducao="props.traducao"
+    ></dialogo>
+  </template>
+
+  <script>
+  export default {
+    props: {
+      original: {
+        type: String,
+        required: true
+      },
+      traducao: {
+        type: String,
+        required: true
+      }
+    }
+  }
+  </script>
